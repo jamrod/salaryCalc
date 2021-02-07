6364cfc4abae0f069b7ac00f62cda2c868908efa
Jamrod <myfavoritenoisemaker@gmail.com>
2021-02-07 16:38:46 -0700

    added readme and has been deployed

---
+++ README.md
@@ -0,0 +1,20 @@
+# salarycalc
+
+salarycalc is a simple tool to figure yearly salary from an hourly or weekly wage and vice versa. It is intended to be installed and run from the command line.
+
+## Installation
+
+npm -g salarycalc
+
+### Useage
+
+salarycalc is intended as a cli module so you would invoke it from the terminal. Make sure you install with the '-g' flag so that the command 'salarycalc' is added.
+Pass the amount and the units as arguments like so 'salaryCalc (num) (unit)'
+where (num) is a dollar amount and (unit) indicates whether (num) is the hourly, 'h', weekly, 'w', or yearly, 'y', salary.
+Example:
+If you want to know how much $30.00 an hour converts to as a yearly and weekly salary type this command:
+'salarycalc 30 h' returns '$62,400.00 per anum, $1,200.00 per week'
+Yearly is assumed to be in thousands so '85 y' is the same as '85000 y'
+You can add an optional third argument for hours worked per week if not the standard 40 so you can calculate for part-time employment.
+Example:
+'salarycalc 500 w 30' returns '$16.67 per hour, $26,000.00 per anum'"
--- index.js
+++ index.js
@@ -1,25 +1,63 @@
+const helpText = "salarycalc is a simple tool to convert between hourly, weekly and yearly salary. \nPass the amount and the units as arguments like so 'salaryCalc (num) (unit)'\nwhere (num) is the dollar amount and (unit) indicates whether num is the hourly, 'h', weekly, 'w', or yearly, 'y', salary.\nExample: 'salarycalc 30 h' returns '$62,400.00 per anum, $1,200.00 per week' \nYearly is assumed to be in thousands so '85 y' is the same as '85000 y'\nYou can add an optional third argument for hours worked per week if not the standard 40.\nExample: 'salarycalc 500 w 30' returns '$16.67 per hour, $26,000.00 per anum'"
+
 function calc(args) {
 
-    let num = parseFloat(args[0])
+    let weeklyHours = 40
+
+    if (isNaN(args[0])) {
+        console.log(helpText)
+        return
+    }
+
+    if (args[2]) {
+        weeklyHours = parseInt(args[2])
+    }
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })
+
+    let num = parseFloat(args[0]).toFixed(2)
+    if (num < 0) {
+        console.log("Please enter a positive number.\n", helpText)
+        return
+    }
 
     hourlyToAnnual = (n) => {
-        let week = n * 40
+        let week = n * weeklyHours
         let annual = week * 52
-        return `${annual} per anum, ${week} per week`
+        return `${formatter.format(annual)} per anum, ${formatter.format(week)} per week`
     }
     annualToHourly = (n) => {
         let week = n / 52
-        let hour = week / 40
-        return `${hour} per hour, ${week} per week`
+        let hour = week / weeklyHours
+        return `${formatter.format(hour)} per hour, ${formatter.format(week)} per week`
     }
 
-    if (args[1] == 'y') {
-        num = num * 1000
-        console.log(annualToHourly(num))
-    } else if (args[1] == 'h') {
-        console.log(hourlyToAnnual(num))
-    } else {
-        console.log("Invalid entry \nEnter in this format '(num) (unit)'\nwhere (num) is the dollar amount and (unit) is 'h' to indicate num is per hour amount or 'y' indicates num is per anum amount")
+    weeklyToAnnual = (n) => {
+        let hour = n / weeklyHours
+        let annual = n * 52
+        return `${formatter.format(hour)} per hour, ${formatter.format(annual)} per anum`
     }
+
+    let unit = args[1].toLowerCase()
+
+    if (unit == 'y') {
+        if (num / 1000 < 1) {
+            num = num * 1000
+        }
+        console.log(annualToHourly(num))
+    } else if (unit == 'h') {
+        console.log(hourlyToAnnual(num))
+    } else if (unit == 'w') {
+        console.log(weeklyToAnnual(num))
+    } else {
+        console.log("Invalid entry\n", helpText)
+    }
+    return null
 }
+
+
 module.exports = calc
